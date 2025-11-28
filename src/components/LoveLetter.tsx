c8c7aae3c8fb1ebc59fb11d4557b6b498badc8ac
AbdallahGado <abdallahgado22@gmail.com>
2025-11-28 22:49:20 +0200

    0.7

--- src/components/LoveLetter.tsx
+++ src/components/LoveLetter.tsx
@@ -1,6 +1,6 @@
-"use client";
-
 /* eslint-disable react/no-unescaped-entities */
+
+"use client";
 
 import { motion, AnimatePresence } from "framer-motion";
 import { useInView } from "framer-motion";
